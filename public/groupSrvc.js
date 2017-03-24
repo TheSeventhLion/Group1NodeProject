50756c869e890aa3b78607e5cc7b140b64eeba14
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-03-24 10:13:31 -0600

    more basic js

--- public/groupSrvc.js
+++ public/groupSrvc.js
@@ -1,3 +1,10 @@
-angular.module('groupApp').service('groupSrvc', function(http) {
+angular.module('groupApp').service('groupSrvc', function($http) {
+
+  this.getUsers = function() {
+    return $http( {
+      method: 'GET',
+      url: 'http://192.168.3.129:4000/api/data'
+    });
+  };
 
 });
