4403da1b45b6b1f2873a2e762097e01658baab44
Nathaniel-j-s <nathaniel.j.mith@gmail.com>
2017-03-24 11:47:18 -0600

    finished with a question mark

--- public/groupCtrl.js
+++ public/groupCtrl.js
@@ -1,18 +1,14 @@
 angular.module('groupApp').controller('groupCtrl', function($scope, groupSrvc) {
 
-  $scope.test = 'test';
-
   $scope.users;
 
   $scope.getUsers = groupSrvc.getUsers().then(function(response) {
-      console.log(response);
-      $scope.users = response;
+      $scope.users = response.data;
   });
 
   $scope.deleteUser = function(id) {
     groupSrvc.deleteUser(id).then(function(response) {
       $scope.users = response.data;
-      console.log(response);
     });
   };
 
