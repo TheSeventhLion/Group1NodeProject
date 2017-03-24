8c3a88c8aa504be8cbbd0d250821901f1cb062ae
Aaron Bowers <aaronjbowers@gmail.com>
2017-03-24 10:29:56 -0600

    test commit

--- public/groupCtrl.js
+++ public/groupCtrl.js
@@ -1,8 +1,8 @@
 angular.module('groupApp').controller('groupCtrl', function($scope, groupSrvc) {
-
   $scope.getUsers = function() {
     groupSrvc.getUsers().then(function(response) {
       $scope.users = response.data;
+      console.log($scope.user);
     });
   };
 
